a75040975564e073f749e1834045b65fca616502
Eric Tobin <eatobin@gmail.com>
2021-08-03 18:02:35 -0500

    tester

--- C1/copy1.c
+++ C1/copy1.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
 
 /* copy input to output; 1st version */
-main()
-{
-	int c;
+//Ctl + D = EOF
 
-	c = getchar();
-	while (c != EOF) {
-		putchar(c);
-		c = getchar();
-	}
+int main() {
+    char c;
+
+    c = (char) getchar();
+    while (c != EOF) {
+        putchar(c);
+        c = (char) getchar();
+    }
+    return 0;
 }
+
+//#include <stdio.h>
+//
+//int main() {
+//    int c;
+//
+//    while ((c = getchar()) != '\n') putchar(c);
+//    printf("\nEOF = %i", EOF);
+//    printf("\n(EOF == -1) is %i (true)", (EOF == -1));
+//    printf("\n(EOF != -1) is %i (false)", (EOF != -1));
+//    return 0;
+//}
---
+++ tester.c
@@ -0,0 +1,23 @@
+//* typeSize.c -- prints out type sizes */
+#include <stdio.h>
+
+int main() {
+    /* c99 provides a %zd specifier for sizes */
+    printf("Type int has a size of %zd bytes.\n", sizeof(int));
+    printf("Type char has a size of %zd bytes.\n", sizeof(char));
+    printf("Type EOF has a size of %zd bytes.\n", sizeof(EOF));
+    printf("Type long has a size of %zd bytes.\n", sizeof(long));
+    printf("Type long long has a size of %zd bytes.\n",
+           sizeof(long long));
+    printf("Type double has a size of %zd bytes.\n",
+           sizeof(double));
+    printf("Type long double has a size of %zd bytes.\n",
+           sizeof(long double));
+    printf("Type float has a size of %zd bytes.\n", sizeof(float));
+    int a = 0;
+    double d = 10.21;
+    printf("0 + 10.21 (%f) has a size of %zd bytes.\n", (a + d),
+           sizeof(a + d));
+//    printf("%f", a + d);
+    return 0;
+}
