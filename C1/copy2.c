8ad014e3922230cab9f8ff96be3695fb842874ce
Eric Tobin <eatobin@gmail.com>
2021-08-05 15:21:51 -0500

    copy2.c

--- C1/copy2.c
+++ C1/copy2.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
 
 /* copy input to output; 2nd version */
-main()
-{
-	int c;
+//main()
+//{
+//	int c;
+//
+//	while ((c = getchar()) != EOF)
+//		putchar(c);
+//}
 
-	while ((c = getchar()) != EOF)
-		putchar(c);
+int main() {
+    FILE *fp;
+    int c;
+
+    fp = freopen("../resources/file.txt", "r", stdin);
+
+    while ((c = fgetc(fp)) != EOF)
+        putchar(c);
+
+    fclose(fp);
+
+    return 0;
 }
