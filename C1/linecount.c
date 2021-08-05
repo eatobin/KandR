a0f681107161f248263b6146e4dc4b6df312736f
Eric Tobin <eatobin@gmail.com>
2021-08-05 16:22:07 -0500

    line counting

--- C1/linecount.c
+++ C1/linecount.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
 /* count lines in input */
-main()
-{
-	int c, nl;
+main() {
+    FILE *fp;
+    int c, nl;
 
-	nl = 0;
-	while ((c = getchar()) != EOF)
-		if (c == '\n')
-			++nl;
-	printf("%d\n", nl);
+    fp = freopen("../resources/file.txt", "r", stdin);
+
+    nl = 0;
+    while ((c = fgetc(fp)) != EOF)
+        if (c == '\n')
+            ++nl;
+    printf("%d\n", nl);
+
+    fclose(fp);
 }
