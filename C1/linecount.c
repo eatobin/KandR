d20eb69994480db85b800c80b7087766f86914c2
Eric Tobin <eatobin@gmail.com>
2023-01-23 21:59:03 -0700

    linecount.c

--- C1/linecount.c
+++ C1/linecount.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 
 /* count lines in input */
-int main()
-{
-    FILE *fp;
-    int c, nl;
+int main() {
+  FILE *fp;
+  int   c, nl;
 
-    fp = freopen("resources/file.txt", "r", stdin);
+  fp = freopen("resources/file.txt", "r", stdin);
 
-    nl = 0;
-    while ((c = fgetc(fp)) != EOF)
-        if (c == '\n')
-            ++nl;
-    printf("%d\n", nl);
+  nl = 0;
+  while ((c = fgetc(fp)) != EOF)
+    if (c == '\n') ++nl;
+  printf("%d\n", nl);
 
-    fclose(fp);
+  fclose(fp);
 
-    return 0;
+  return 0;
 }
