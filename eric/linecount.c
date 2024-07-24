8e9754be893dbfe50868a05eb8534aba0ff9ccab
Eric Tobin <eatobin@gmail.com>
2024-07-23 22:13:56 -0700

    new eric

---
+++ eric/linecount.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/* count lines in input */
+int main() {
+    FILE *fp;
+    int c, nl;
+
+    fp = freopen("file.txt", "r", stdin);
+
+    nl = 0;
+    while ((c = fgetc(fp)) != EOF)
+        if (c == '\n') ++nl;
+    printf("%d\n", nl);
+
+    fclose(fp);
+
+    return 0;
+}
